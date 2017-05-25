16d020b0e061422b2c8c68c4e7b96460dd82d026
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-05-25 12:29:48 -0400

    Initisal commit

---
+++ spotify.js
@@ -0,0 +1,24 @@
+'use strict';
+var getFromApi = function(endpoint, query={}) {
+  const url = new URL(`https://api.spotify.com/v1/${endpoint}`);
+  Object.keys(query).forEach(key => url.searchParams.append(key, query[key]));
+  return fetch(url).then(function(response) {
+    if (!response.ok) {
+      return Promise.reject(response.statusText);
+    }
+    return response.json();
+  });
+};
+
+
+var artist;
+var getArtist = function(name) {
+  return getFromApi('search', {
+    q: name,
+    limit: 1,
+    type:'artist'
+  }).then(item => {
+    artist = item.artists.items[0];
+    return artist;
+  });
+};
